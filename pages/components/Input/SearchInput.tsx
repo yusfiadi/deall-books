a6c19f97ae12ee3e44ab13661066c3424eff2c9e
Yusfi Adilaksa <yadilaksa@gmail.com>
2022-05-26 23:12:19 +0700

    delete unused code

--- pages/components/Input/SearchInput.tsx
+++ pages/components/Input/SearchInput.tsx
@@ -1,7 +1,6 @@
-import React, { useState } from "react";
+import React from "react";
 import IconButton from "@mui/material/IconButton";
 import OutlinedInput from "@mui/material/OutlinedInput";
-import InputLabel from "@mui/material/InputLabel";
 import InputAdornment from "@mui/material/InputAdornment";
 import FormControl from "@mui/material/FormControl";
 import SearchIcon from "@mui/icons-material/Search";
