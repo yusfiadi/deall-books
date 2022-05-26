98c9de9cf0921a3bddf51bbf43adbd2a3ee2238e
Yusfi Adilaksa <yadilaksa@gmail.com>
2022-05-26 23:08:34 +0700

    create simple navbar to navigate from home page and bookmark page

---
+++ pages/components/AppBar/AppBar.tsx
@@ -0,0 +1,105 @@
+import React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import IconButton from "@mui/material/IconButton";
+import Typography from "@mui/material/Typography";
+import Menu from "@mui/material/Menu";
+import AccountCircle from "@mui/icons-material/AccountCircle";
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import Tooltip from "@mui/material/Tooltip";
+import MenuItem from "@mui/material/MenuItem";
+import Link from "next/link";
+
+const pages = ["Bookmark"];
+
+const ResponsiveAppBar = () => {
+  const [anchorElNav, setAnchorElNav] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
+
+  const handleOpenNavMenu = (event: any) => {
+    setAnchorElNav(event.currentTarget);
+  };
+  const handleOpenUserMenu = (event: any) => {
+    setAnchorElUser(event.currentTarget);
+  };
+
+  const handleCloseNavMenu = () => {
+    setAnchorElNav(null);
+  };
+
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
+
+  return (
+    <AppBar position="static" color="transparent">
+      <Container maxWidth="md">
+        <Toolbar disableGutters>
+          <Typography
+            variant="h6"
+            noWrap
+            component="div"
+            sx={{
+              flexGrow: 1,
+              display: "flex",
+            }}
+          >
+            <Link href="/" passHref>
+              <a>Deall-Books</a>
+            </Link>
+          </Typography>
+          <Box sx={{ flexGrow: 0, display: { xs: "flex", sm: "none" } }}>
+            <Tooltip title="Open settings">
+              <IconButton onClick={handleOpenUserMenu} sx={{ p: 1 }}>
+                <AccountCircle />
+              </IconButton>
+            </Tooltip>
+            <Menu
+              sx={{ mt: "45px" }}
+              id="menu-appbar"
+              anchorEl={anchorElUser}
+              anchorOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              keepMounted
+              transformOrigin={{
+                vertical: "top",
+                horizontal: "right",
+              }}
+              open={Boolean(anchorElUser)}
+              onClose={handleCloseUserMenu}
+            >
+              {pages.map((page) => (
+                <Link href="/bookmark" key={page} passHref>
+                  <a>
+                    <MenuItem onClick={handleCloseUserMenu}>
+                      <Typography textAlign="center">{page}</Typography>
+                    </MenuItem>
+                  </a>
+                </Link>
+              ))}
+            </Menu>
+          </Box>
+          <Box sx={{ flexGrow: 0, display: { xs: "none", sm: "flex" } }}>
+            {pages.map((page) => (
+              <Link href="/bookmark" key={page} passHref>
+                <a>
+                  <Button
+                    onClick={handleCloseNavMenu}
+                    sx={{ my: 2, color: "black", display: "block" }}
+                  >
+                    {page}
+                  </Button>
+                </a>
+              </Link>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+};
+export default ResponsiveAppBar;
