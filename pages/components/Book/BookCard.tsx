369b5b16f5a7708dffbf13aff6a30f0533b4725e
Yusfi Adilaksa <yadilaksa@gmail.com>
2022-05-26 23:10:25 +0700

    adjust book card component style looks like sejuta cita booku style

--- pages/components/Book/BookCard.tsx
+++ pages/components/Book/BookCard.tsx
@@ -4,6 +4,7 @@ import CardContent from "@mui/material/CardContent";
 import CardMedia from "@mui/material/CardMedia";
 import Typography from "@mui/material/Typography";
 import styled from "@emotion/styled";
+
 import BookModal from "./BookModal";
 
 type Props = {
@@ -20,12 +21,24 @@ type Props = {
     }[];
     audio_length: number;
   };
+  myBooks: any;
+  handleBookmark: (book: any) => void;
+  handleUnbookmark: (title: string) => void;
+  isBookmarkPage?: boolean;
 };
 
-const CustomTypography = styled(Typography)`
+const BookTitleStyled = styled(Typography)`
   white-space: nowrap;
   overflow: hidden;
   text-overflow: ellipsis;
+  color: rgba(0, 0, 0, 1);
+`;
+
+const BookAuthorStyled = styled(Typography)`
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  color: rgba(0, 0, 0, 0.5);
 `;
 
 const BookCard = (props: Props) => {
@@ -34,27 +47,37 @@ const BookCard = (props: Props) => {
   const handleClose = () => setOpen(false);
   return (
     <>
-      <Card onClick={handleOpen} style={{ cursor: "pointer" }}>
+      <Card
+        onClick={handleOpen}
+        style={{ cursor: "pointer", boxShadow: "none" }}
+      >
         <CardMedia
           component="img"
-          width={100}
-          height={150}
+          width={400}
+          height={600}
           image={props.book.cover_url}
           alt={props.book.title}
+          style={{ height: "100%", objectFit: "initial", borderRadius: "10px" }}
         />
-        <CardContent>
-          <CustomTypography gutterBottom variant="body1">
-            {props.book.title}
-          </CustomTypography>
-          <CustomTypography variant="body2" color="text.secondary">
+        <CardContent style={{ padding: "10px 0" }}>
+          <BookTitleStyled variant="body1">{props.book.title}</BookTitleStyled>
+          <BookAuthorStyled variant="body2" color="text.secondary">
             {props.book.authors[0]}
             {props.book.authors.length === 2 &&
               `, and ${props.book.authors[1]}`}
             {props.book.authors.length > 2 && `, and others}`}
-          </CustomTypography>
+          </BookAuthorStyled>
         </CardContent>
       </Card>
-      <BookModal book={props.book} open={open} handleClose={handleClose} />
+      <BookModal
+        book={props.book}
+        open={open}
+        handleClose={handleClose}
+        myBooks={props.myBooks}
+        handleBookmark={(book: any) => props.handleBookmark(book)}
+        handleUnbookmark={(title: string) => props.handleUnbookmark(title)}
+        isBookmarkPage={props.isBookmarkPage}
+      />
     </>
   );
 };
