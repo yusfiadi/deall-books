814531e132220d71206dc00eb38fc9bbb252a3e6
Yusfi Adilaksa <yadilaksa@gmail.com>
2022-05-26 23:06:28 +0700

    create search input component, the keyword and handle change function are from props

---
+++ pages/components/Input/SearchInput.tsx
@@ -0,0 +1,41 @@
+import React, { useState } from "react";
+import IconButton from "@mui/material/IconButton";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import InputLabel from "@mui/material/InputLabel";
+import InputAdornment from "@mui/material/InputAdornment";
+import FormControl from "@mui/material/FormControl";
+import SearchIcon from "@mui/icons-material/Search";
+
+type Props = {
+  keyword: string;
+  handleChange: (event: any) => void;
+};
+
+const SearchInput = (props: Props) => {
+  console.log(props.keyword);
+  return (
+    <FormControl sx={{ width: "100%", my: 3 }} variant="outlined">
+      <OutlinedInput
+        id="search-keyword"
+        type={"text"}
+        placeholder="Search this category"
+        value={props.keyword}
+        size={"small"}
+        onChange={props.handleChange}
+        startAdornment={
+          <InputAdornment position="start" style={{ marginRight: "0" }}>
+            <IconButton edge="start">
+              <SearchIcon />
+            </IconButton>
+          </InputAdornment>
+        }
+        style={{
+          borderRadius: "10px",
+        }}
+        color="primary"
+      />
+    </FormControl>
+  );
+};
+
+export default SearchInput;
